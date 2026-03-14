62a93d9d4fdfea4f91b8251cc4b166aae42664eb
Pierre le Riche <pierre@cimso.com>
2026-03-14 12:07:44 +0200

    Fix a potential integer overflow in the usage tracker under 64-bit

--- Demos/Usage Tracker/FastMMUsageTracker.pas
+++ Demos/Usage Tracker/FastMMUsageTracker.pas
@@ -506,7 +506,7 @@ var
       if AMemoryMap[LInd] = csExSysAllocated then
       begin
         {Get all the reserved memory blocks and windows allocated memory blocks, etc.}
-        VirtualQuery(Pointer(LInd * 65536), LMBI, SizeOf(LMBI));
+        VirtualQuery(Pointer({$ifdef WIN64}Int64{$endif}(LInd) * 65536), LMBI, SizeOf(LMBI));
         if LMBI.State = MEM_COMMIT then
         begin
           if (GetModuleFileName(DWord(LMBI.AllocationBase), LA_Char, MAX_PATH) <> 0) then
